3864e7061bc780a94f024a5f7b4a0fb6490aff34
Adam Berenik <adam.berenik@gmail.com>
2023-10-14 12:01:43 +0200

    fork-tracker changed to use KRETFUNC instead of KRETPROBE

--- fork-tracker.py
+++ fork-tracker.py
@@ -18,13 +18,10 @@ static void remember_fork(u32 parent_pid, u32 child_pid, char val)
     bpf_trace_printk("parent %d - fork return value: %d\\n", parent_pid, child_pid);
 }
 
-int trace_fork(struct pt_regs *ctx)
+KRETFUNC_PROBE(kernel_clone, void* args, int child_pid)
 {
     char _TRUE = 1, _FALSE = 0;
     u32 parent_pid = bpf_get_current_pid_tgid();
-
-    // return value of the fork system call
-    u32 child_pid = PT_REGS_RC(ctx);
 
     if (parent_pid == #DEFAULT_PID#)
     {
@@ -62,9 +59,6 @@ ebpf_text = ebpf_text.replace("#DEFAULT_PID#", str(default_pid))
 
 # Load the eBPF program
 b = BPF(text=ebpf_text)
-
-# Attach the program to the clone return
-b.attach_kretprobe(event=b.get_syscall_fnname("clone"), fn_name="trace_fork")
 
 while True:
     try:
