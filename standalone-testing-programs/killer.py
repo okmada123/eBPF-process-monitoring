d4c01dd23059cbf01ee4340a085be330b16e783f
Adam Berenik <adam.berenik@gmail.com>
2023-11-21 17:10:00 +0100

    killer now tracks forks and kills children xd

--- standalone-testing-programs/killer.py
+++ standalone-testing-programs/killer.py
@@ -1,29 +1,84 @@
 #!/usr/bin/python3
 
 from bcc import BPF
+#!/usr/bin/python3
 
-b = BPF(text="""
-#include <uapi/linux/ptrace.h>
+from bcc import BPF
+import sys
+
+# eBPF program code
+ebpf_text = """
+#include <linux/ptrace.h>
 #include <linux/signal.h>
+#define _TRUE 1
+#define _FALSE 2 // static helper functions apparently cannot return 0
 
-int syscall__execve(struct pt_regs *ctx, const char *filename)
+BPF_HASH(tracked_pids, u32, char);
+
+static int is_tracked_pid(u32 pid)
 {
-    u32 pid = bpf_get_current_pid_tgid();
-    if (pid >= 6000 & pid <= 10000)
+    if (pid == #DEFAULT_PID#) return _TRUE;
+    
+    char* map_value = tracked_pids.lookup(&pid);
+    if (map_value == NULL || *map_value == _FALSE)
     {
-        long rv = bpf_send_signal(SIGKILL);
-        bpf_trace_printk("exec killed ? rv: %d\\n", rv);
-        return 0;
+        return _FALSE;
     }
+
+    return _TRUE;
+}
+
+static void remember_fork(u32 parent_pid, u32 child_pid, char val)
+{
+    // remember child value in the map so that we can track it
+    tracked_pids.insert(&child_pid, &val);
+}
+
+KRETFUNC_PROBE(kernel_clone, void* args, int child_pid)
+{
+    u32 parent_pid = bpf_get_current_pid_tgid();
+    if (is_tracked_pid(parent_pid) != _TRUE) return 0;
     else
     {
-        char buff[256];
-        int rv = bpf_probe_read_user(buff, sizeof(buff), filename);
-        bpf_trace_printk("pid: %d, buffer: %s\\n", pid, buff);
-
+        remember_fork(parent_pid, child_pid, _TRUE);
+        // Print info
+        bpf_trace_printk("parent %d - fork return value: %d\\n", parent_pid, child_pid);
         return 0;
     }
 }
-""")
+
+int syscall__execve(struct pt_regs *ctx)
+{
+    u32 pid = bpf_get_current_pid_tgid();
+    if (is_tracked_pid(pid) != _TRUE) return 0;
+
+    long rv = bpf_send_signal(SIGKILL);
+    bpf_trace_printk("exec killed rv: %d\\n", rv);
+    return 0;
+}
+
+"""
+
+if (len(sys.argv) != 2):
+    print("Usage: ./killer.py <DEFAULT-PID>")
+    exit(1)
+
+try:
+    default_pid = int(sys.argv[1])
+    if default_pid <= 0: raise Exception
+except:
+    print("<DEFAULT-PID> has to be a valid PID (positive integer)")
+    exit(1)
+
+ebpf_text = ebpf_text.replace("#DEFAULT_PID#", str(default_pid))
+
+# Load the eBPF program
+b = BPF(text=ebpf_text)
 b.attach_kprobe(event=b.get_syscall_fnname("execve"), fn_name="syscall__execve")
-b.trace_print()
+
+while True:
+    try:
+        print(b.trace_readline())
+    except KeyboardInterrupt:
+        break
+
